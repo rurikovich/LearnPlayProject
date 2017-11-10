a029cab17bc3d74de93d4c65679e9af3d9c62810
RastegaevYO <RastegaevYO@sovcombank.ru>
2017-11-10 12:11:56 +0400

    delete unused controllers

--- test/FunctionalSpec.scala
+++ test/FunctionalSpec.scala
@@ -12,39 +12,6 @@ import play.api.test.Helpers._
   */
 class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
 
-  "Routes" should {
-
-    "send 404 on a bad request" in {
-      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
-    }
-
-    "send 200 on a good request" in {
-      route(app, FakeRequest(GET, "/")).map(status(_)) mustBe Some(OK)
-    }
-
-  }
-
-  "HomeController" should {
-
-    "render the index page" in {
-      val home = route(app, FakeRequest(GET, "/")).get
-
-      status(home) mustBe Status.OK
-      contentType(home) mustBe Some("text/html")
-    }
-
-  }
-
-  "CountController" should {
-
-    "return an increasing count" in {
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
-    }
-
-  }
-
   "FigureController" should {
 
     "correct json" in {
